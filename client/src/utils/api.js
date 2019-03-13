652d6c249219031aae08769dcd4356a5948070ba
Christian Dorado <christian.dorado@orchard.com.au>
2019-03-13 19:11:13 +1100

    Update api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -2,7 +2,7 @@
 
 class Api {
     async getCharacters(search, limit, offset){
-      const query = await fetch('http://localhost:3001/api/characters?nameStartsWith='+search+'&limit='+limit+'&offset='+offset);
+      const query = await fetch('/api/characters?nameStartsWith='+search+'&limit='+limit+'&offset='+offset);
       const  data  = await query.json();
       return data
     }
